b5660fb05bb9e2d52a1401323b0e8bee8e919155
Harshavardhan Reddy <reddyharsha1437@gmail.com>
2024-08-12 20:55:00 +0530

    initial commit - Project setup

---
+++ src/test/java/com/example/mobile_prepaid_recharge/MobilePrepaidRechargeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mobile_prepaid_recharge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobilePrepaidRechargeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
